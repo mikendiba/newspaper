62ee1ac2fb00fce62b467d0567c7482cae4852d4
mikendiba <mikendiba@gmail.com>
2019-06-11 22:56:27 +0300

    Added staff status in user list display

--- newspaper_project/users/admin.py
+++ newspaper_project/users/admin.py
@@ -10,7 +10,9 @@ class CustomerUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
+    list_display = ['email','username','age','is_staff']
 
-    admin.site.register(CustomUser, UserAdmin)
+
+admin.site.register(CustomUser, CustomerUserAdmin)
 
 # Register your models here.
